7d7b7fb1fea53a9a07ce6aa85f84d4b88162ab00
Josh Smith <racersmith@gmail.com>
2018-03-13 05:10:29 -0700

    Change the reward for takeoff task
    - Reward for height gained and positive vertical velocity
    - Penalty for velocity component parallel to ground
    - Penalty for angular velocity

--- task.py
+++ task.py
@@ -28,7 +28,21 @@ class Task():
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        # reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+
+        # Position Reward
+        # reward = 1.0/(np.linalg.norm(self.sim.pose[:3] - self.target_pos) + 1.0)
+
+        # Takeoff Reward
+        reward = self.sim.pose[2]/self.target_pos[2]
+        # reward for fast vertical travel
+        reward += 0.1*self.sim.v[2]
+        # penalty for non-goal related velocity
+        reward -= 0.1*np.log(np.linalg.norm(self.sim.v[:2])+1.0)
+
+        # Angular velocity penalty
+        reward -= 0.1*np.log(np.linalg.norm(self.sim.angular_v)+1.0)
+
         return reward
 
     def step(self, rotor_speeds):
